32815e2ac41df9d60e6d96798a64183a305cde52
Carlos <00002721@uca.edu.sv>
2024-04-23 13:53:12 -0600

    Add: CORS

--- Back-end/auth/src/main/java/com/uca/auth/controllers/AuthController.java
+++ Back-end/auth/src/main/java/com/uca/auth/controllers/AuthController.java
@@ -6,10 +6,7 @@ import com.uca.auth.models.auth.RegisterRequest;
 import com.uca.auth.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,12 +15,14 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
         return ResponseEntity.ok(authService.login(request));
 
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
         return ResponseEntity.ok(authService.register(request));
